3745efc789f29dbc569659a8a9f83093747a4f98
Pavel Prokofyev <pavel.prokofyev@outlook.com>
2015-05-01 10:57:38 +0500

    Added one agrument functionality

--- Calculator.cs
+++ Calculator.cs
@@ -18,7 +18,8 @@ namespace Kontur.Courses.Git
 				//> 4
 				// + 1
 				//>5
-				return lastResult = Maybe<double>.FromError("Not implemented yet");
+                var v1 = double.Parse(args[1]);
+			    return lastResult = Execute(args[0], lastResult.Value, v1);
 			}
 			if (args.Length == 3)
 			{
